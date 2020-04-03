6be643269101c6fa112c931918b46fd0b515fa71
Face70 <60823541+DarkKnight70@users.noreply.github.com>
2020-04-03 17:57:54 -0600

    rewrote the javascript, finally got the image to play and pause when clicked

--- assets/javascript/giphyjs.js
+++ assets/javascript/giphyjs.js
@@ -1,59 +1,61 @@
+var topics = ["Batman", "The League", "Deadpool", "Party Time", "Superman", "This is the End", "Covid-19"];
+    
 $(document).ready(function() {
 
-
-    var topics = ["Batman", "The League", "Deadpool", "Party Time", "Superman", "This is the End", "Covid-19"];
+function createButtons() {
     
-    function displayGif() {
-        var apiKey = "zQ9cvPUPVYGG0yHzaLmUuFwz7v7Iq5zi";
-        var input = $(this).attr("data");
-        var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + input + "&api_key=" + apiKey + "&limit=10";
+    $("#buttons").empty()
 
-        $.ajax({
-        url: queryURL,
-        method: "GET"
-        }).then(function(response){
-        
-        console.log(response);
-        
-        var results = response.data;    
-        for (var i = 0; i < results.length; i++) {
-        var newDiv = $("<div class='newgif'>");
-        var text = $("<p>");
-        var newImage = $('<img src= " ' + results[i].images.fixed_height_still.url + ' " data-still=" ' + results[i].images.fixed_height_still.url + ' " data-animate=" ' + results[i].images.fixed_height.url + ' " data-state = "still" class= "movImage" style = "width:250px; height:250px;">');
-        
-        $(text).text("Rating: " + results[i].rating);
-        //$(newImage).attr("src", results[i].images.fixed_height_still.url);
-        $(newDiv).append(text, newImage);
-
-        $(".gifs-here").prepend(newDiv); 
-        } 
-        });
-    }
+    for(var i = 0; i < topics.length; i++) {
     
-
-
-    function createButtons() {
-
-        $("#buttons").empty()
-
-        for(var i = 0; i < topics.length; i++) {
-        
-            var newButton = $("<button>");
-            newButton.addClass("gif");
-            newButton.attr("data", topics[i]);
-            newButton.text(topics[i]);
-            $("#buttons").append(newButton);
-        }
+        var newButton = $("<button>");
+        newButton.addClass("gifbutton");
+        newButton.attr("data", topics[i]);
+        newButton.text(topics[i]);
+        $("#buttons").append(newButton);
     }
+}
 
-    $("#add-item").on("click", function(event) {
-        event.preventDefault();
-        var newInput = $("#input").val();
-        topics.push(newInput);
-        createButtons()
-    })
-
-    $(document).on("click", ".gif", displayGif);
-
+$("#add-item").on("click", function(event) {
+    event.preventDefault();
+    var newInput = $("#input").val();
+    topics.push(newInput);
     createButtons()
+})
+
+function displayGif() {
+    var gifName = $(this).attr("data");
+    $.ajax({
+            url: 'https://api.giphy.com/v1/gifs/search?q= ' + gifName + ' &api_key=zQ9cvPUPVYGG0yHzaLmUuFwz7v7Iq5zi&limit=10',
+            type: 'GET',
+        })
+        .done(function(response) {
+           
+    for (var i = 0; i < response.data.length; i++) {
+        var rating = "<div class='ratings'> Rating:  " + (response.data[i].rating) + " </div>";
+        var image = rating + '<img src= " ' + response.data[i].images.fixed_height_still.url +
+            '" data-still=" ' + response.data[i].images.fixed_height_still.url +
+            ' " data-animate=" ' + response.data[i].images.fixed_height.url + '" data-state="still" class="movImage">';
+
+        image = '<div class="newgif">' + image + "</div>";
+        $('.gifs-here').prepend(image);
+    }
+
+    $('.movImage').on('click', function() {
+        var state = $(this).attr('data-state');
+        if (state == 'still') {
+            $(this).attr('src', $(this).attr("data-animate"));
+            $(this).attr('data-state', 'animate');
+        } else {
+            $(this).attr('src', $(this).attr("data-still"));
+            $(this).attr('data-state', 'still');
+        }
+
+    });
+});
+}
+
+$(document).on("click", ".gifbutton", displayGif);
+    
+        createButtons()
 });
